0e62d4507c09699bb0dfaa19b272a36031280c70
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-03-17 19:58:38 +0530

    created home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,25 @@
 import { useNavigate } from "react-router-dom";
-
-import supabase from "../supabase";
+import styles from "./Home.module.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  const logoutHander = async () => {
-    const data = await supabase.auth.signOut();
-
-    if (!data.error) alert("Logout success");
-  };
-
   return (
-    <>
-      <h1>Home</h1>
-      <button onClick={() => navigate("/login")}>Login</button>
-      <button onClick={logoutHander}>Logout</button>
-    </>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={`animate__bounceInLeft ${styles.heading}`}>
+          Sport Match
+        </h1>
+        <span className={styles.about}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
+          dolorum ea cum voluptatum iure voluptate error molestiae a recusandae
+          nemo, tempora dolores eaque. Commodi?
+        </span>
+        <button className={styles.button} onClick={() => navigate("/login")}>
+          get started
+        </button>
+      </div>
+    </div>
   );
 };
 
