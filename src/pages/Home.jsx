b44da1da4e60b7a6dd4b8716bc984b05fcc462c2
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-03-18 16:55:44 +0530

    Add tailwind on basic styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,13 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={`animate__bounceInLeft ${styles.heading}`}>
+    <div className="h-screen h-screen bg-black bg-no-repeat bg-center bg-cover 
+    ">
+      <div className="h-full w-2/5 flex flex-col pr-24 justify-center gap-16 pl-24">
+        <h1 className={`animate__bounceInLeft uppercase text-7xl font-bold text-slate-50`}>
           Sport Match
         </h1>
-        <span className={styles.about}>
+        <span className="font-normal text-slate-50 text-2xl tracking-wide leading-normal">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
           dolorum ea cum voluptatum iure voluptate error molestiae a recusandae
           nemo, tempora dolores eaque. Commodi?
