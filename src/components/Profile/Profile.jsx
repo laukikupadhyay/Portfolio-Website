36fb472cb8ff64b393e7d9086e4e751a6c2669ff
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-03-21 00:24:17 +0530

    Profile card Modify

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faGear  } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser,faLocationDot , faBriefcase ,faGear} from "@fortawesome/free-solid-svg-icons";
+import twitter from '../../assests/icons/twitter.svg'
 import styles from './Profile.module.css'
 
 function Profile() {
@@ -9,24 +9,26 @@ function Profile() {
     <div className={styles.profileCard}>
         <div className={styles.picname}>
             <div className={styles.nameRight}>
-            <img className={styles.avatar} src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar'/>
+            <FontAwesomeIcon className={styles.avatar} icon={faUser} />
             <div className={styles.self}>
             <div className={styles.name}>Anshu Joshi</div>
             <div className={styles.friends}>0 friends</div>
             </div>
             </div>
             <div className={styles.settings}>
-            <FontAwesomeIcon icon="fa-solid fa-gear" />
+            <FontAwesomeIcon icon={faGear} />
             </div>
         </div>
         <div className={styles.divider}></div>
         <div className={styles.details}>
             <div className={styles.location}>
-                <img src='http://www.clker.com/cliparts/0/9/7/1/1194984989151900001map-marker.svg' alt='location'/>
+                <div>
+                <FontAwesomeIcon className={styles.icon} icon={faLocationDot}/>
+                </div>
                 <div className={styles.locationText}>Location</div>
             </div>
             <div className={styles.interests}>
-                <img src='https://www.flaticon.com/svg/static/icons/svg/149/149222.svg' alt='interests'/>
+            <FontAwesomeIcon className={styles.icon} icon={faBriefcase}/>
                 <div className={styles.interestsText}>Interests</div>
             </div>
         </div>
@@ -37,7 +39,7 @@ function Profile() {
                 <div className='who-viewed-count'>0</div>
             </div>
             <div className={styles.impressions}>
-                <div className='impressions-text'>Profile views</div>
+                <div className={styles.impressionsText}>Profile views</div>
                 <div className='impressions-count'>0</div>
             </div>
         </div>
@@ -45,11 +47,12 @@ function Profile() {
         <div className='social-profile'>
             <div className={styles.socialProfileText}>Social profile</div>
             <div className={styles.socialProfileIcons}>
-            {/* <FontAwesomeIcon icon={faGear} />
+                {/* <img src={twitter}/> */}
             <FontAwesomeIcon icon={faGear} />
             <FontAwesomeIcon icon={faGear} />
-            <FontAwesomeIcon icon={faGear}/> */}
-            <FontAwesomeIcon icon="fa-solid fa-gear" />
+            <FontAwesomeIcon icon={faGear} />
+            <FontAwesomeIcon icon={faGear}/>
+            {/* <FontAwesomeIcon icon="fa-solid fa-gear" /> */}
             </div>
         </div>
     </div>
