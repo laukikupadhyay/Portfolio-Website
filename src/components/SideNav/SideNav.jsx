6be0f5462704a7ba538b1a8a0773b79c86ce23c2
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-04-01 05:15:41 +0530

    Link components to side nav

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -3,17 +3,45 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
-function SideNav() {
+function SideNav(props) {
   return (
     <div className={styles.sideNav}>
-            <div className={styles.link}>My Account</div>
-            <div className={styles.link}>Profile</div>
-            <div className={styles.link}>Create Room</div>
-            <div className={styles.link}>Manage Rooms</div>
-            <div className={styles.link}>Manage Friends</div>
-            <div className={styles.diversion}></div>
-            <div className={styles.link}>Log out       <FontAwesomeIcon className={styles.logOutIcon} icon={faRightFromBracket} /></div>
+    <div
+      className={styles.link}
+      onClick={() => props.onLinkClick("MyAccount")}
+    >
+      My Account
     </div>
+    <div
+      className={styles.link}
+      onClick={() => props.onLinkClick("EditProfile")}
+    >
+      Profile
+    </div>
+    <div
+      className={styles.link}
+      onClick={() => props.onLinkClick("CreateRoom")}
+    >
+      Create Room
+    </div>
+    <div
+      className={styles.link}
+      onClick={() => props.onLinkClick("MyRooms")}
+    >
+      Manage Rooms
+    </div>
+    <div
+      className={styles.link}
+      onClick={() => props.onLinkClick("Friends")}
+    >
+      Manage Friends
+    </div>
+    <div className={styles.diversion}></div>
+    <div className={styles.link}>
+      Log out{" "}
+      <FontAwesomeIcon className={styles.logOutIcon} icon={faRightFromBracket} />
+    </div>
+  </div>
   )
 }
 
--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../../components/navbar/Navbar";
 import SideNav from "../../components/SideNav/SideNav";
 import MyAccount from "../../components/MyAccount/MyAccount";
@@ -9,18 +9,23 @@ import CreateRoom from "../../components/Rooms/CreateRoom/CreateRoom";
 import EditProfile from "../../components/EditProfile/EditProfile";
 
 function Main() {
+  const [activeComponent, setActiveComponent] = useState("EditProfile");
+
+  function handleLinkClick(componentName) {
+    setActiveComponent(componentName);
+  }
   return (
-    <>
-    <Navbar />
-    <div style={{ display: "flex", flexDirection: "row" }}>
-    <SideNav/>
-    {/* <MyAccount/> */}
-    {/* <MyRooms/> */}
-    {/* <Friends/> */}
-    {/* <CreateRoom/> */}
-    <EditProfile/>
+    <div >
+      <Navbar />
+      <div className={styles.userlayout}>
+        <SideNav onLinkClick={handleLinkClick} />
+        {activeComponent === "MyAccount" && <MyAccount />}
+        {activeComponent === "MyRooms" && <MyRooms />}
+        {activeComponent === "Friends" && <Friends />}
+        {activeComponent === "CreateRoom" && <CreateRoom />}
+        {activeComponent === "EditProfile" && <EditProfile />}
+      </div>
     </div>
-    </>
   );
 }
 
