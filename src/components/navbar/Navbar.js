62ef98b7c9240c1b25034e5b7d5ed6f46872c88e
Prasun <prasunmondal60@gmail.com>
2023-04-01 12:33:51 +0530

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,39 +1,76 @@
-import React from "react";
-import styles from "./Navbar.module.css";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { TbMoonFilled } from "react-icons/tb";
+import { IoMdNotifications } from "react-icons/io";
 
-function Navbar() {
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
-    <div className={styles.nav}>
-      <div className={styles.navheader}>
-        <div className={styles.navtitle}>Logo</div>
-      </div>
-      <div className={styles.navbtn}>
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="#" className="nav-logo logo-container">
+            <span class="Logo">Meet n Play</span>
+          </NavLink>
 
-      <div className={styles.navlinks}>
-        <a href="#" >
-          Link1
-        </a>
-        <a href="#" >
-          Link 2
-        </a>
-        <a href="#" >
-          Link 3
-        </a>
-        <a href="#" >
-          Link 4
-        </a>
-        <a href="#" >
-          Link 5
-        </a>
-      </div>
-    </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="#"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                <TbMoonFilled />
+              </NavLink>
+            </li>
+            {/* <li className="nav-item">
+              <NavLink
+                exact
+                to="/profile"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Profile
+              </NavLink>
+            </li> */}
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="#"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                <IoMdNotifications />
+              </NavLink>
+            </li>
+            <li >
+              <NavLink
+                exact
+                to="#"
+                activeClassName="active"
+                className="nav-links-buuton"
+                onClick={handleClick}
+              >
+                <span id="container-button">
+                  <button id="button">ROOMS</button>
+                </span>
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
-export default Navbar;
+export default NavBar;
