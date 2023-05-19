9b3a92cb7855f7901e4223efee0821afc9fe47c8
Prasun <prasunmondal60@gmail.com>
2023-05-19 23:58:29 +0530

    payment implemented

--- src/pages/Additem/Additem.js
+++ src/pages/Additem/Additem.js
@@ -3,6 +3,8 @@ import NavBar from "../../components/navbar/Navbar";
 import "./Additem.css"
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function Additem() {
   const userInfo = useSelector((state) => state.userInfo);
   const [image, setImage] = useState();
@@ -11,7 +13,6 @@ function Additem() {
   const [cat, setCat] = useState("");
   const [itemprice, setItemprice] = useState("");
   const navigate = useNavigate();
-
 
   const handleSubmit=async(e)=>{
     e.preventDefault();
@@ -24,6 +25,35 @@ function Additem() {
     form.append("image", image);
     form.append("username", userInfo.name);
     form.append("email", userInfo.email)
+    
+      const data = await axios.get(process.env.REACT_APP_BACKEND_URL+"razorpay/key");
+      const data1=await axios.post(process.env.REACT_APP_BACKEND_URL + "razorpay/checkout",{
+        amount: itemprice,
+      });
+      console.log(data,data1)
+      const options = {
+        key: data.data.key,
+        amount: data1.data.order.amount,
+        currency: "INR",
+        name: "SportiPHY",
+        description: "TEST Payment",
+        order_id: data1.data.order.id,
+        callback_url:process.env.REACT_APP_BACKEND_URL + "razorpay/paymentverification" ,
+        prefill: {
+            name: "Prasun Mondal",
+            email: "prasunmondal60@gmail.com",
+            contact: "8777045674"
+        },
+        notes: {
+            "address": "Razorpay Corporate Office"
+        },
+        theme: {
+            "color": "#121212"
+        }
+  
+    };
+    
+
     try {
       const response = await fetch(
         process.env.REACT_APP_BACKEND_URL + "items/additem",
@@ -39,13 +69,15 @@ function Additem() {
         setItemdesc("");
         setCat("");
         setItemprice("");      
-        navigate("/buynsell");
+        // navigate("/buynsell");
       })
       .catch((err) => console.log(err));
-      window.location.reload();
     } catch (err) {
       console.log(err);
     }
+    const razor = await new window.Razorpay(options);
+    await razor.open();
+    
   }
   return (
     <div>
